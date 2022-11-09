d649271f12321bd1e12e37d5a59e630ae95d1f7e
tuanhm03 <tuanthanhcong2027@gmail.com>
2022-11-09 14:41:34 +0700

    up_load

---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import { setupCounter } from './counter.js'
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://www.facebook.com/profile.php?id=100014186206636" target="_blank">
+    <img class="anhmoine" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t1.15752-9/313388115_3174035252862173_7248212500748499224_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=uwVUhabtY2sAX9Z51oi&_nc_ht=scontent.fhan17-1.fna&oh=03_AdRyb0VlrT9vkQCzUSKcqpqygUPHvV5velpYUSuAG75C_Q&oe=6392E430" alt="">
+    </a>
+    <h1>Hello! Chào cậu nhé</h1>
+    <div class="card">
+    <a href="grid.html" class="btn-click--see" >Nhấn vào đây có bất ngờ nè</a>
+    </div>
+    <p class="read-the-docs">
+      
+    </p>
+  </div>
+`
+
+// setupCounter(document.querySelector('#counter'))
