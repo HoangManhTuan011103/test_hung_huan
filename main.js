9eda57a214c731bfb2dd5b78c8af2103af2ae434
tuanhm03 <tuanthanhcong2027@gmail.com>
2022-11-09 19:30:27 +0700

    update

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import './style.css'
 // import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://www.facebook.com/profile.php?id=100014186206636" target="_blank">
-    <img class="anhmoine" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t1.15752-9/313388115_3174035252862173_7248212500748499224_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=uwVUhabtY2sAX9Z51oi&_nc_ht=scontent.fhan17-1.fna&oh=03_AdRyb0VlrT9vkQCzUSKcqpqygUPHvV5velpYUSuAG75C_Q&oe=6392E430" alt="">
+    <a href="https://www.facebook.com/hungdeptraivcl21" target="_blank">
+    <img class="anhmoine" src="https://scontent.fhan14-3.fna.fbcdn.net/v/t1.15752-9/314494365_645243977189241_6813469642249712209_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=VSQ8R20viFwAX9bT5B7&_nc_ht=scontent.fhan14-3.fna&oh=03_AdSHMDoC914wiXok9bhtdY61K2W7AmQASaogcMFvq9x-gQ&oe=6393121E" alt="">
     </a>
     <h1>Hello! Chào cậu nhé</h1>
     <div class="card">
